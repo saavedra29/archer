8542ed5f31c09a2b7f7cdc5d790d84dd0d201c10
Aris <arisgold29@gmail.com>
2017-03-10 03:06:23 +0200

    No remaining arrow after hitting the target on creative mode

--- arrow.lua
+++ arrow.lua
@@ -57,6 +57,10 @@ ARCHER_ARROW_ENTITY.on_step = function(self, dtime)
 		for k, obj in pairs(objs) do
 			-- If the object is not player..
 			if obj:get_luaentity() ~= nil then
+				-- -- DEBUG
+				-- if obj:get_luaentity().name == "default:obsidian" then
+				-- 	print("--------HEATING THE TARGET!---------")
+				-- end
 				-- If the object is not arrow && is not something thrown down..
 				if obj:get_luaentity().name ~= "archer:arrow_entity" and obj:get_luaentity().name ~= "__builtin:item" then
 					local damage = 1
@@ -86,9 +90,9 @@ ARCHER_ARROW_ENTITY.on_step = function(self, dtime)
 	if self.lastpos.x ~= nil then
 		-- If not in creative mode don't disappear the arrow after the use. Throw it down
 		if minetest.registered_nodes[node.name].walkable then
-			if not minetest.setting_getbool("creative_mode") then
-				minetest.add_item(self.lastpos, "archer:arrow")
-			end
+			-- if not minetest.setting_getbool("creative_mode") then
+			-- 	minetest.add_item(self.lastpos, "archer:arrow")
+			-- end
 			minetest.sound_play("archer_arrow", {pos = self.lastpos, gain = 0.8})
 			self.object:remove()
 		end
